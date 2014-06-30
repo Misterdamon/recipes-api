8fd73d717b79004cc63619255e2fd22c29264938
Jake Persing <misterdamon@gmail.com>
2014-06-29 23:33:01 -0700

    deleted duplicated app

--- recipes_api/app/models/key.rb
+++
@@ -1,7 +0,0 @@
-  class Key < ActiveRecord::Base
-    attr_accessible :key_num
-
-      # def as_json(options={})
-      #   super(:only => [:title])
-      # end
-  end
--- recipes_api/app/models/recipe.rb
+++
@@ -1,6 +0,0 @@
-  class Recipe < ActiveRecord::Base
-    attr_accessible :title, :content
-      # def as_json(options={})
-      #   super(:only => [:title])
-      # end
-  end
